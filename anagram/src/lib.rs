fed7b2aee999c9b0248f786c44f40741e2980893
Yasen Dimitrov <y_dimitrov@ymail.com>
2025-12-30 12:32:19 +0200

    sync anagram

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,5 +1,20 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut output = HashSet::from_iter([]);
+    let mut temp_word: Vec<char> = word.to_lowercase().chars().collect();
+    temp_word.sort_unstable();
+    let sorted_word: String = temp_word.into_iter().collect();
+
+    for item in possible_anagrams {
+        let mut temp: Vec<char> = item.to_lowercase().chars().collect();
+        temp.sort_unstable();
+        let sorted: String = temp.into_iter().collect();
+
+        if sorted == sorted_word {
+            output.insert(*item);
+        }
+    }
+
+    output
 }

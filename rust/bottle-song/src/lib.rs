274c762fb4c778eaf64edccabcf49b02492dcad6
Yasen Dimitrov <y_dimitrov@ymail.com>
2026-01-05 22:27:12 +0200

    sync bottle-song

--- rust/bottle-song/src/lib.rs
+++ rust/bottle-song/src/lib.rs
@@ -1,3 +1,67 @@
+const FULL_TEXT: &str = "Ten green bottles hanging on the wall,
+Ten green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be nine green bottles hanging on the wall.
+
+Nine green bottles hanging on the wall,
+Nine green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be eight green bottles hanging on the wall.
+
+Eight green bottles hanging on the wall,
+Eight green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be seven green bottles hanging on the wall.
+
+Seven green bottles hanging on the wall,
+Seven green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be six green bottles hanging on the wall.
+
+Six green bottles hanging on the wall,
+Six green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be five green bottles hanging on the wall.
+
+Five green bottles hanging on the wall,
+Five green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be four green bottles hanging on the wall.
+
+Four green bottles hanging on the wall,
+Four green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be three green bottles hanging on the wall.
+
+Three green bottles hanging on the wall,
+Three green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be two green bottles hanging on the wall.
+
+Two green bottles hanging on the wall,
+Two green bottles hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be one green bottle hanging on the wall.
+
+One green bottle hanging on the wall,
+One green bottle hanging on the wall,
+And if one green bottle should accidentally fall,
+There'll be no green bottles hanging on the wall.";
+
 pub fn recite(start_bottles: u32, take_down: u32) -> String {
-    todo!("Return the bottle song starting at {start_bottles} and taking down {take_down} bottles")
+    let mut temp: Vec<&str> = FULL_TEXT.split("\n\n").collect();
+    temp.reverse();
+
+    let actual_start = start_bottles - take_down;
+    let actual_end = start_bottles;
+
+    let output: Vec<&str> = temp
+        .get(actual_start as usize..actual_end as usize)
+        .unwrap()
+        .iter()
+        .rev()
+        .copied()
+        .collect();
+
+    output.join("\n\n")
 }

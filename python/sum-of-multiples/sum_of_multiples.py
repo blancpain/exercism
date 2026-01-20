97129f01d9e935f4ca0e40f9b5f3c58a0055199a
Yasen Dimitrov <y_dimitrov@ymail.com>
2026-01-20 22:01:49 +0200

    sync sum of multiples completion

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,10 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    unique_multiples = set()
+
+    for multiple in multiples:
+        current_multiples = [
+            x for x in range(1, limit) if multiple != 0 and x % multiple == 0
+        ]
+        unique_multiples.update(current_multiples)
+
+    return sum(unique_multiples)

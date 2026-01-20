e07f4af62ca449db60a1e332ff94b1e6ad5b30c5
Yasen Dimitrov <y_dimitrov@ymail.com>
2026-01-20 16:01:57 +0200

    sync darts completion

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,11 @@
-def score(x, y):
-    pass
+def score(x: float, y: float) -> int:
+    calc: float = pow(x, 2) + pow(y, 2)
+
+    if calc <= 1**2:
+        return 10
+    if calc <= 5**2:
+        return 5
+    if calc <= 10**2:
+        return 1
+
+    return 0
---
+++ python/sum-of-multiples/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sum-of-multiples!")
+
+
+if __name__ == "__main__":
+    main()
